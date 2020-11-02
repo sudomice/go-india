05b6d5787acc8a1d1581ac315a18be2646bb260d
sakshamagarwal <saksham.agarwal@tesco.com>
2020-11-02 13:05:47 +0530

    Add validations for ticket

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import os
 
 
 app = Flask(__name__)
-# app.config.from_object(os.environ['APP_SETTINGS'])
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# from models import Result
+from models import Result
 
 
     
