e0be6006158ccf72f60b65e583ca092cc6ec7eed
sakshamagarwal <saksham.agarwal@tesco.com>
2020-11-02 12:57:37 +0530

    Add structure for collect

--- app.py
+++ app.py
@@ -11,9 +11,29 @@ db = SQLAlchemy(app)
 from models import Result
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+    
+
+    
+
+
+
+@app.route('/collect')
+def collect():
+    # Endpoint to collect tickets
+    response = requests.get(
+        "https://gpay.app.goo.gl/" + code)
+    
+    if "Dynamic Link Not Found" in response.text:
+        return "Invalid ticket"
+    
+    html = bs4.BeautifulSoup(
+        response.text, features="lxml")
+    if "A special" in html.title.text:
+        print("https://gpay.app.goo.gl/" + code)
+        city = html.title.text.split(" ")[2]
+        # TODO: Add to DB as unclaimed ticket if doesn't exist
+
+    
 
 
 if __name__ == '__main__':
